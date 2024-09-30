6474b5f5289a1380411d806767a19fa41892a075
mutabazi-00 <mutajean30@gmail.com>
2024-09-30 19:03:11 +0000

    Add block scoped

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,8 @@
 export default function taskBlock(trueOrFalse) {
-  var task;
-  var task2;
 
   
-  task = false;
-  task2 = true;
+  let task = false;
+  let task2 = true;
 
 
   return [task, task2];
