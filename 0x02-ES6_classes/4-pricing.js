6ea0263acb5a3c2cfcae7c2551c8d718ac58a36e
mutabazi-00 <mutajean30@gmail.com>
2024-10-03 18:23:41 +0000

    Pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,46 @@
+import Currency from './3-currency';
+
+class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  // Getter for amount
+  get amount() {
+    return this._amount;
+  }
+
+  // Setter for amount
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = value;
+  }
+
+  // Getter for currency
+  get currency() {
+    return this._currency;
+  }
+
+  // Setter for currency
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of Currency');
+    }
+    this._currency = value;
+  }
+
+  // Method to display full price
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  // Static method to convert price
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
+
+export default Pricing;
