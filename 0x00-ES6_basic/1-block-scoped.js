1191e2196be36400e040bd8673d4d0744e32699a
mutabazi-00 <mutajean30@gmail.com>
2024-10-04 14:16:21 +0000

    Block scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,6 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    // No need to re-declare task and task2 inside this block
+    // Just update their values directly (but remember we can't reassign `const`)
+  }
 
   return [task, task2];
 }
