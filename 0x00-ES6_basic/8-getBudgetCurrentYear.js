8ff938151913dc2129e6186eee7ecfeb557b24b5
mutabazi-00 <mutajean30@gmail.com>
2024-10-04 14:47:23 +0000

    Adding in properties

---
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -0,0 +1,15 @@
+function getCurrentYear() {
+  const date = new Date();
+  return date.getFullYear();
+}
+
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  const year = getCurrentYear(); // Store the current year for reuse
+  const budget = {
+    [`income-${year}`]: income,
+    [`gdp-${year}`]: gdp,
+    [`capita-${year}`]: capita,
+  };
+
+  return budget;
+}
