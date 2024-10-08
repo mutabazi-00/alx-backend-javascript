cedaa97f1b1e8471af539d6041cc3da3b790969b
mutabazi-00 <mutajean30@gmail.com>
2024-10-08 18:05:27 +0000

    Edit clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,11 +2,14 @@ const cleanSet = (set, startString) => {
   // Return an empty string if startString is empty
   if (startString === '') return '';
 
-  // Filter, map and join the set values
+  // Ensure the input is a Set
+  if (!(set instanceof Set)) return '';
+
+  // Filter, map, and join the set values
   return Array.from(set)
     .filter((value) => value.startsWith(startString))
-    .map((value) => value.slice(startString.length)) // Append the rest of the string
-    .join('-'); // Join with '-'
+    .map((value) => value.slice(startString.length)) // Get the rest of the string after startString
+    .join('-'); // Join the results with '-'
 };
 
 export default cleanSet;
